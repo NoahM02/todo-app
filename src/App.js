0157b6a6524a93cad76ef31afcf244aee375b596
NoahM02 <marschal.noah02@gmail.com>
2022-03-18 18:49:03 +0100

    plain-react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <form>
+        <input type="text"></input>
+      </form>
+    </div>
+  );
+}
+
+export default App;
