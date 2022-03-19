d67b53df9a11241440612853b81645a05f204006
NoahM02 <marschal.noah02@gmail.com>
2022-03-19 13:50:48 +0100

    working functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
+import { useReducer, useState } from 'react';
 import './App.css';
+import ToDo from './ToDo';
+
+export const ACTIONS = {
+  ADD: 'add',
+  REMOVE: 'remove',
+  TOGGLE: 'toggle',
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD:
+      return action.payload.name !== ''
+        ? [...todos, newToDo(action.payload.name)]
+        : todos;
+    case ACTIONS.TOGGLE:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+  }
+}
+
+function newToDo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD, payload: { name: name } });
+    setName('');
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <form>
-        <input type="text"></input>
+      <h1>Todo-App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
       </form>
+      {todos.map((todo) => {
+        return <ToDo key={todo.id} todo={todo} dispatch={dispatch} />;
+      })}
     </div>
   );
 }
